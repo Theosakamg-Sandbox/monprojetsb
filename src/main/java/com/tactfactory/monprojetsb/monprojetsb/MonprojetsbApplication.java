fdf1d72655be9a066475a6756218005e9a9583cd
Mickael Gaillard <mickael.gaillard@tactfactory.com>
2021-03-05 12:12:51 +0100

    Initial commit

---
+++ src/main/java/com/tactfactory/monprojetsb/monprojetsb/MonprojetsbApplication.java
@@ -0,0 +1,20 @@
+package com.tactfactory.monprojetsb.monprojetsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonprojetsbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonprojetsbApplication.class, args);
+    }
+
+    public void toto() {
+
+    }
+
+    public void titi() {
+
+    }
+}
